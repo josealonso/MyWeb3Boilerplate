e68f64408b22fb447e8112d95c587d328f0362eb
josealonso <jose.alonso.programmer@gmail.com>
2022-07-04 13:58:08 +0200

    Configure desired chains, generate connectors and setup a "wagmi" client

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,12 +1,35 @@
+// Configure your desired chains and generate the required connectors.
+// You will also need to setup a wagmi client.
+//...
 import '@rainbow-me/rainbowkit/styles.css';
 
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { chain, configureChains, createClient, WagmiConfig, defaultChains } from 'wagmi';
+import { InjectedConnector } from '@wagmi/core';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
-
 import '../styles/globals.css'
+
+const { chains, provider } = configureChains(
+  // If a user has their wallet connected to a chain that is unsupported by your app, the provider will use the first chain listed in the chains array.
+  // [chain.polygonMumbai, chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [chain.hardhat, chain.polygonMumbai, chain.polygon],
+  [
+    alchemyProvider({ alchemyId: process.env.POLYGON_ALCHEMY_ID }),
+    // The publicProvider ensures that your chains always have an RPC URL to fall back on (in case Alchemy does not support the chain).
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: [new InjectedConnector({ chains })],
+  provider
+})
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
