6e4b85d94bc2b759b49236c4d55033d157cfdb10
josealonso <jose.alonso.programmer@gmail.com>
2022-07-06 13:34:43 +0200

    Layout with two sections

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Container, Divider, Flex, VStack } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
-import Image from 'next/image';
 import AddTxButton from '../components/AddTxButton';
 import MyConnectButton from '../components/MyConnectButton';
 import MyFooter from '../components/MyFooter';
@@ -11,17 +10,20 @@ import styles from '../styles/Home.module.css'
 // export default function Home(): AppProps {
 export default function Home() {
   return (
-    <Container maxH="container.xl" p={0}>
+    <Container maxW="container.xl" p={0}>
+      <MyConnectButton />
       <MyHeader />
       <Divider p={5} />
-      <MyConnectButton />
-      <Flex h="100vh" py={20}>
-        <VStack>
-          <h3>Hola a todos</h3>
+      <Flex h="100vh" py={10}>
+        <VStack w="full" h="full" p={10} spacing={10}
+          alignItems="flex-start">
+          <h3>First section</h3>
           <AddTxButton />
-          <VStack>
-            <MyFooter />
-          </VStack>
+        </VStack >
+        <VStack w="full" h="full" p={10} spacing={10}
+          alignItems="flex-start" bg="gray.50">
+          <h3>Second section</h3>
+          <MyFooter />
         </VStack>
       </Flex>
     </Container>
