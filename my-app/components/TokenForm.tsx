c8de1c3052f6700ccb797630a4200bb242d95787
josealonso <jose.alonso.programmer@gmail.com>
2022-07-14 18:22:19 +0200

    Some corrections

--- my-app/components/TokenForm.tsx
+++ my-app/components/TokenForm.tsx
@@ -3,31 +3,44 @@ import {
     FormErrorMessage, FormLabel, FormControl,
     Box, Flex, Heading, Input, Button,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export default function TokenForm() {
-    const {
-        handleSubmit,
-        register,
-        formState: { errors, isSubmitting },
-    } = useForm()
+    // const {
+    //     handleSubmit,
+    //     register,
+    //     formState: { errors, isSubmitting },
+    // } = useForm()
 
+    const [name, setName] = useState("");
+    const [symbol, setSymbol] = useState("");
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert(`Name: ${name} AND Symbol: ${symbol}`);
+    };
     return (
         <Flex width="full" align="center" justifyContent="center">
             <Box p={2}>
                 <Box textAlign="center">
                     <Heading>Token Details</Heading>
                 </Box>
-                <Box my={4} textAlign="left">
-                    <form>
-                        <FormControl>
+                <Box p={8} maxW="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
+                    <form onSubmit={handleSubmit}>
+                        <FormControl isRequired>
                             <FormLabel>Token Name</FormLabel>
-                            <Input type="text" placeholder="My Token" />
+                            <Input type="text" placeholder="My Token" 
+                            size="lg"
+                            onChange={event => setName(event.currentTarget.value)}
+                            />
                         </FormControl>
-                        <FormControl mt={6}>
+                        <FormControl isRequired mt={6}>
                             <FormLabel>Token Symbol</FormLabel>
-                            <Input type="type" placeholder="MYT" />
+                            <Input type="type" placeholder="MYT" 
+                            size="lg"
+                            onChange={event => setSymbol(event.currentTarget.value)}
+                            />
                         </FormControl>
-                        <Button width="full" mt={4} type="submit">
+                        <Button width="full" variant="outline" color="teal" borderColor="teal" maxWidth="full" mt={4} type="submit">
                             Generate
                         </Button>
                     </form>
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Container, Divider, Flex, VStack } from '@chakra-ui/react';
+import { Container, Divider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import AddTxButton from '../components/AddTxButton';
-import MyForm from '../components/MyForm';
 import MyConnectButton from '../components/MyConnectButton';
 import MyFooter from '../components/MyFooter';
 import MyHeader from '../components/MyHeader';
 
 import styles from '../styles/Home.module.css'
+import TokenForm from '../components/TokenForm';
 
 // export default function Home(): AppProps {
 export default function Home() {
@@ -15,14 +15,15 @@ export default function Home() {
       <MyConnectButton />
       <MyHeader />
       <Divider p={5} />
-      <Flex h="100vh" py={10}>
-        <MyForm />
+      {/* <Flex h="100vh" py={10}> */}
+        <TokenForm />
+        {/* <MyForm />
         <VStack w="full" h="full" p={10} spacing={10}
           alignItems="flex-start">
           <h3>Second section</h3>
           <MyFooter />
-        </VStack>
-      </Flex>
+        </VStack> */}
+      {/* </Flex> */}
     </Container>
   )
 }
