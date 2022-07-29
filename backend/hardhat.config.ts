3899f9d3f82398c54426727ec16e8cb6cd7e9dac
JR <jose.alonso.programmer@gmail.com>
2022-07-29 17:07:00 +0200

    Enable optimizer in hardhat config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -5,13 +5,27 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        /**
+         * Optimize for how many times you intend to run the code.
+         * Lower values will optimize more for initial deployment cost, higher
+         * values will optimize more for high-frequency usage.
+         * @see https://docs.soliditylang.org/en/v0.8.6/internals/optimizer.html#opcode-based-optimizer-module
+         */
+        runs: 1000,
+      },
+    },
+  },
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      // forking: {
-      //   url: process.env.MUMBAI_API_KEY_URL || "",
-      // },
+      forking: {
+        url: process.env.MUMBAI_API_KEY_URL || "",
+      },
       chainId: 1337   // We set 1337 to make interacting with MetaMask simpler
     },
     mumbai: {
